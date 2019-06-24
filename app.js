650cb421caee6847574ab6f37955a1d9eab9df09
tubbodeTang <txb_dev@163.com>
2019-06-24 14:53:01 +0800

    let express render the page

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var express = require('express');
-const exStatic = require("express-static");
+var path = require('path');
+// const exStatic = require("express-static");
+
+var ejs = require('ejs');
+
 
 var app = express();
 
+app.set('views', path.join(__dirname, './'));
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
 
-app.use(exStatic('./'));
+// app.use(exStatic('./'));
 
-// app.get('/', function (request, response, next) {
+app.get('/', function (request, response, next) {
+    response.type('html');
+    response.render('jiming');
+})
 
-
-// }
+//app.
 
 app.listen(process.env.PORT || 3000, function () {
     console.log("app is running at port 3000");
